46c63710f3ea2d51906c477b07335e86e1949eeb
João Felipe de Lima <joao.lima@levva.io>
2023-05-19 17:35:42 -0300

    Estilização - Modal IV

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,39 @@
-import { HeaderContainer, HeaderContent, NewCategoryButton, NewTransactionButton, UserAvatar } from "./styles";
+import { ReactNode } from "react";
 
-import levvaCoinsLogo from "../../assets/logo.svg"
+import levvaCoinsLogo from "../../assets/logo.svg";
 
 import FotoPerfil from "../../assets/perfil.png"
+import { Modal } from "../Modal";
+import { HeaderContainer, HeaderContent, NewCategoryButton, NewTransactionButton, UserAvatar } from "./styles";
+import { Form, FormInput, FormButton } from "../../styles/global";
 
 export function Header() {
+    const newCategoryButton: ReactNode = (<NewCategoryButton>Nova Categoria</NewCategoryButton>);
+    const newTransactionButton: ReactNode = (<NewTransactionButton>Nova Transação</NewTransactionButton>);
+    const userAvatar: ReactNode = (<UserAvatar src={FotoPerfil} />);
+
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={levvaCoinsLogo} alt="levva coins" />
 
                 <div>
-                    <NewCategoryButton>Nova Categoria</NewCategoryButton>
-                    <NewTransactionButton>Nova Transação</NewTransactionButton>
-                </div>
-            </HeaderContent>
+                    <Modal title="Nova Categoria" trigger={newCategoryButton}>
 
-            <UserAvatar src={FotoPerfil} />
+                    </Modal>
+                    <Modal title="Nova Transação" trigger={newTransactionButton}>
+
+                    </Modal>
+                </div>
+                <Modal title="Meu perfil" trigger={userAvatar}>
+                    <Form>
+                        <UserAvatar src={FotoPerfil} alt="foto de perfil" variant="large" />
+                        <FormInput type="name" value="João Felipe" />
+                        <FormInput type="email" placeholder="joao.lima@levva.io" disabled />
+                        <FormButton type="submit">Atualizar</FormButton>
+                    </Form>
+                </Modal>
+            </HeaderContent>
         </HeaderContainer>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const HeaderContainer = styled.header`
     background: linear-gradient(to bottom, ${props => props.theme.black}, ${props => props.theme["gray-600"]});
@@ -65,7 +65,11 @@ export const NewTransactionButton = styled.button`
     }
 `;
 
-export const UserAvatar = styled.img`
+interface UserAvatarProps {
+    variant?: "large";
+}
+
+export const UserAvatar = styled.img<UserAvatarProps>`
     width: 3.5rem;
     height: 3.5rem;
     border-radius: 50%;
@@ -83,4 +87,10 @@ export const UserAvatar = styled.img`
         border: 2px solid ${props => props.theme["yellow-500"]};
         box-shadow: 0px 0px 10px 20px #00000015;
     }
+
+    ${(props => props.variant === "large" && css`
+        width: 8rem;
+        height: 8rem;
+        margin-bottom: 1rem;
+    `)}
 `;
