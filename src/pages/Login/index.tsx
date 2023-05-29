ff6e29ff6ba97bf6c7c5ac02c1d66020d8089cab
João Felipe de Lima <joao.lima@levva.io>
2023-05-29 14:06:03 -0300

    React + .NET - Aula 02

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,37 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import { AuthLayout } from "../../layouts/AuthLayout";
-import { Form, FormButton, FormInput, Link } from "../../styles/global";
+import { Form, FormButton, FormError, FormInput, Link } from "../../styles/global";
+
+interface FormProps {
+    email: string;
+    password: string;
+}
+
+const formSchema = yup.object({
+    email: yup.string().email("Digite um e-mail válido").required("O e-mail é obrigatório"),
+    password: yup.string().required("A senha é obrigatória")
+})
+    .required();
 
 export function Login() {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormProps>({
+        resolver: yupResolver(formSchema)
+    })
+
+    function handleLogin({ email, password }: FormProps) {
+        console.log({ email, password });
+    }
+
     return (
         <AuthLayout title="Login" subtitle="Gerenciar suas entradas e saídas nunca foi tão simples.">
-            <Form>
-                <FormInput type="email" placeholder="E-mail" />
-                <FormInput type="password" placeholder="Senha" />
+            <Form onSubmit={handleSubmit(handleLogin)}>
+                <FormInput {...register("email")} placeholder="E-mail" />
+                {errors.email && <FormError>{errors.email.message}</FormError>}
+                <FormInput {...register("password")} type="password" placeholder="Senha" />
+                {errors.password && <FormError>{errors.password.message}</FormError>}
                 <Link>Não tem conta? Cadastre-se aqui.</Link>
                 <FormButton type="submit">Entrar</FormButton>
             </Form>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -22,15 +22,15 @@ export const GlobalStyle = createGlobalStyle`
     body, input, button {
         font: 400 1rem 'Roboto', sans-serif;
     }
-`
+`;
 
 export const Form = styled.form`
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: start;
 
     padding-top: 1rem;
-`
+`;
 
 export const FormInput = styled.input`
     background: ${props => props.theme["gray-700"]};
@@ -44,7 +44,12 @@ export const FormInput = styled.input`
     & + & {
         margin-top: 1rem;
     }
-`
+`;
+
+export const FormError = styled.span`
+    color: ${props => props.theme["red-500"]};
+    margin: 0.5rem 0 1rem;
+`;
 
 export const FormButton = styled.button`
     background: ${props => props.theme["yellow-300"]};
@@ -77,7 +82,7 @@ export const Link = styled.a`
         cursor: pointer;
         text-decoration: underline;
     }
-`
+`;
 interface TransactionTypeButtonProps {
     variant: "income" | "outcome";
 }
