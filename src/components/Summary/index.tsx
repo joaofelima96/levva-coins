c11a4105fe15144a9065584abacb1df141a03142
João Felipe de Lima <joao.lima@levva.io>
2023-06-07 15:21:03 -0300

    fix: Somatória dos cards

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -12,11 +12,11 @@ export function Summary() {
     const summary = transactions.reduce(
         (acc, transaction) => {
             if (transaction.type === 1) {
-                acc.deposits += transaction.amount;
-                acc.total += transaction.amount;
-            } else {
                 acc.withdraws += transaction.amount;
                 acc.total -= transaction.amount;
+            } else {
+                acc.deposits += transaction.amount;
+                acc.total += transaction.amount;
             }
 
             return acc;
