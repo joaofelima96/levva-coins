e310d047a6fae74bd4d1e8904a884cb989a59e9b
João Felipe de Lima <joao.lima@levva.io>
2023-06-16 17:42:05 -0300

    feat: Conexão com backEnd

--- src/services/CategoryServices/CategoryService.ts
+++ src/services/CategoryServices/CategoryService.ts
@@ -9,7 +9,7 @@ const createCategory = async ({
     description,
 }: NewCategoryParams): Promise<CategoryValues> => {
     return Api.post({
-        url: "/category",
+        url: "/Category/Create",
         body: {
             description,
         },
@@ -24,7 +24,7 @@ const createCategory = async ({
 
 const getCategories = async (): Promise<CategoryValues[]> => {
     return Api.get({
-        url: "/category/list",
+        url: "/Category/list",
     })
         .then((response) => {
             return response.data
